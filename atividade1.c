ea7fe58727afaf61837952cd97ebb380733c2858
LuisFim <llwg@cesar.school>
2026-06-16 13:36:35 -0300

    Add price increase calculation in atividade1.c

--- atividade1.c
+++ atividade1.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main() {
+    float preco = 100.0f;
+    float *ptr_preco = &preco;
+
+    printf("Preco antes do aumento: R$ %.2f\n", preco);
+
+    *ptr_preco = *ptr_preco * 1.10f;
+
+    printf("Preco depois do aumento: R$ %.2f\n", preco);
+
+    return 0;
+}
