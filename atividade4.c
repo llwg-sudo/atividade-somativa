2f60b22ec2d494dcd1feaf075b93d2ffc0a031eb
LuisFim <llwg@cesar.school>
2026-06-16 13:38:17 -0300

    Update atividade4.c

--- atividade4.c
+++ atividade4.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
 
+typedef struct {
+    int id;
+    float preco;
+} Produto;
+
+void aplicar_desconto(Produto *produto, float percentual_desconto) {
+    produto->preco = produto->preco - (produto->preco * percentual_desconto / 100.0f);
+}
+
+void imprimir_produtos(Produto *ptr, int quantidade) {
+    int i;
+
+    for (i = 0; i < quantidade; i++) {
+        printf("Produto %d\n", i + 1);
+        printf("ID: %d\n", (ptr + i)->id);
+        printf("Preco: R$ %.2f\n\n", (ptr + i)->preco);
+    }
+}
+
+int main() {
+    Produto estoque[3] = {
+        {1, 100.0f},
+        {2, 250.0f},
+        {3, 80.0f}
+    };
+
+    aplicar_desconto(&estoque[0], 10.0f);
+
+    imprimir_produtos(estoque, 3);
+
+    return 0;
+}
